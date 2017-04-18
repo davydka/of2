19e82bcdc99b69a6c4cb88e4fd342c09017ab291
davydka <davydka@gmail.com>
2017-04-18 17:24:13 -0400

    Are fbos cool? not sure.

--- bin/data/shadersGL3/shader.frag
+++ bin/data/shadersGL3/shader.frag
@@ -2,6 +2,9 @@
 
 // this is how we receive the texture
 uniform sampler2DRect tex0;
+uniform sampler2DRect tex1;
+uniform sampler2DRect tex3;
+uniform sampler2DRect tex4;
 
 in vec2 texCoordVarying;
 
@@ -9,7 +12,7 @@ out vec4 outputColor;
  
 void main()
 {
-    //outputColor = texture(tex0, texCoordVarying);
-	vec4 tex = texture ( tex0, texCoordVarying );
-	gl_FragColor = vec4(tex.r,tex.g,tex.b,1.0);
+    outputColor = texture(tex0, texCoordVarying);
+	//vec4 tex = texture ( tex0, texCoordVarying );
+	//gl_FragColor = vec4(tex.r,tex.g,tex.b,1.0);
 }
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -15,36 +15,42 @@ void ofApp::setup(){
 
 	movieMovie.setPixelFormat(OF_PIXELS_NATIVE);
 	//movieMovie.setPixelFormat(OF_PIXELS_I420);
-	movieMovie.load("trailer_1080p.mp4");
+	movieMovie.load("trailer_320.mp4");
 	movieMovie.setLoopState(OF_LOOP_NORMAL);
-	movieMovie.play();
+	//movieMovie.play();
 
 	ofPixels & pixels = movieMovie.getPixels();
 	int vidWidth = pixels.getWidth();
 	int vidHeight = pixels.getHeight();
 
 	shader.load("shadersGL3/shader");
-	img.load("images/img.jpg");
-	plane.set(480, 270, 10, 10);
-	plane.mapTexCoords(0, 0, vidWidth, vidHeight);
-	fbo.allocate(vidWidth, vidHeight);
+	img.load("images/green.png");
+	//plane.set(480, 270, 10, 10);
+	plane.set(1920, 1080, 10, 10);
+	//plane.mapTexCoords(0, 0, 1920, 1080);
+	plane.mapTexCoords(0, 0, 1920, 1080);
+	//plane.mapTexCoords(0, 0, vidWidth, vidHeight);
+	//fbo.allocate(vidWidth, vidHeight);
+	fbo.allocate(1920, 1080);
+	//fbo.allocate(480, 270);
 
 
 
 
 
-	/*
 	movieMovie2.setPixelFormat(OF_PIXELS_NATIVE);
 	//movieMovie.setPixelFormat(OF_PIXELS_I420);
-	movieMovie2.load("trailer_1080p_inverted.mp4");
+	movieMovie2.load("trailer_320_inverted.mp4");
 	movieMovie2.setLoopState(OF_LOOP_NORMAL);
+	movieMovie.play();
 	movieMovie2.play();
+	/*
 
 	shader2.load("shadersGL3/shader");
+	*/
 	plane2.set(480, 270, 10, 10);
 	plane2.mapTexCoords(0, 0, vidWidth, vidHeight);
 	fbo2.allocate(vidWidth, vidHeight);
-	*/
 
 
 
@@ -74,7 +80,7 @@ void ofApp::update(){
 	//tweenValue = ofMap(sin(ofGetElapsedTimef()), -1, 1, 0, 1);
 	//flip++;
 	movieMovie.update();
-	//movieMovie2.update();
+	movieMovie2.update();
 }
 
 //--------------------------------------------------------------
@@ -83,14 +89,43 @@ void ofApp::draw(){
 	fbo.begin();
 		if(movieMovie.isFrameNew()){
 			ofClear(0, 0, 0,255);
+
 			movieMovie.draw(0,0);
+			movieMovie2.draw(200,100);
+
+			movieMovie.draw(500,500);
+			movieMovie2.draw(700,600);
+
+			movieMovie.draw(1500,500);
+			movieMovie2.draw(1700,600);
+
+			img.draw(860, 440);
 		}
+		/*
+		if(movieMovie2.isFrameNew()){
+			ofClear(0, 0, 0,255);
+			movieMovie2.draw(100,100);
+		}
+		*/
 	fbo.end();
+
+	/*
+	fbo2.begin();
+		if(movieMovie2.isFrameNew()){
+			ofClear(0, 0, 0,255);
+			movieMovie2.draw(0,0);
+		}
+	fbo2.end();
+	*/
 	
-	ofTexture tex;
+	ofTexture tex0;
+	//ofTexture tex1;
 	shader.begin();
-	tex = fbo.getTexture();
-	shader.setUniformTexture("tex0", tex, 1);
+	tex0 = fbo.getTexture();
+	tex0.setTextureWrap(GL_REPEAT, GL_REPEAT);
+	//tex1 = fbo2.getTexture();
+	shader.setUniformTexture("tex0", tex0, 1);
+	//shader.setUniformTexture("tex1", tex1, 2);
 	ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
 	plane.draw();
 	shader.end();
